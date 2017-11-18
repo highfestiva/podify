815a25ade656297341c69ade28379b0e62b9f1a7
Jonte <highfestiva@gmail.com>
2017-11-18 21:16:44 +0100

    Download pods into subdirectory

--- podify.py
+++ podify.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 import pafy
 
+try: os.mkdir('pods')
+except: pass
 downloaded = eval(open('downloaded.set').read()) # never download same file twice
 
 for url in open('urls.txt'):
@@ -12,4 +15,6 @@ for url in open('urls.txt'):
     video = pafy.new(url)
     audiostream = max(video.audiostreams, key=lambda audio: abs(audio.rawbitrate-131072))
     filename = audiostream.download()
+    os.rename(filename, 'pods/'+filename)
+    print(filename)
     open('downloaded.set', 'wt').write(str(downloaded))
